5fdb5b5915e2ab15b5264b08bff36ae18a07704d
IsabellaMenendez <70174157+IsabellaMenendez@users.noreply.github.com>
2020-09-04 20:10:42 -0500

    Add files via upload

---
+++ introduction.cc
@@ -0,0 +1,22 @@
+/* 	Isabella Menendez
+	z1903047
+	CSCI 340-2
+	
+	I certify that this is my own work and where appropriate an extension 
+	of the starter code provided for the assignment.
+*/
+#include <iostream>
+
+using namespace std;
+
+int
+main(int argc, char *argv[])
+{
+
+	// Insert new code line replacing this comment.
+	
+	cout << "I'm very excited to be taking Data Structures" << endl;
+	
+	return 0;
+
+}// End of main() routine.
